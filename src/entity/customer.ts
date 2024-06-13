dfd263fab5936d88ec45a6ba608c689eed564273
Gabriel-Macedogmc <gabrielmacedogmc@gmail.com>
2024-06-13 15:53:47 -0300

    feat: value object class address

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,37 @@
+import { Address } from "./address";
+
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
+    _address!: Address;
+    _active: boolean = true;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
     }
 
-    get id(): string {
-        return this._id;
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("ID is required");
+        }
     }
 
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
-    set address(address: string) {
-        this._address = address;
+    activate() {
+        this._active = true;
+    }
+
+    desactivate() {
+        this._active = false;
     }
 }
